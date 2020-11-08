95c3626630eae71d809b23033b2626c100a11ff0
dejongyeong <dejong.re2@gmail.com>
2020-11-08 19:38:32 +0000

    house keeping and refactor test to use describe

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Tutorial: https://www.smashingmagazine.com/2020/07/react-apps-testing-library/
+// refactor to use describe, it pattern:
+describe("<App />", () => {
+  it("Renders <App /> component correctly", () => {
+    const { getByText } = render(<App />);
+    expect(getByText(/learn react/i)).toBeInTheDocument();
+  });
 });
